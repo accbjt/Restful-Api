18a054842d4a85c5994cb719333a3aa6e43daf66
Bill Tran <bjtran1234@gmail.com>
2014-05-03 14:51:39 -0700

    Todos

---
+++ server.js
@@ -0,0 +1,87 @@
+// server.js
+
+// set up ========================
+var express  = require('express');
+var app      = express(); 								// create our app w/ express
+var mongoose = require('mongoose'); 					// mongoose for mongodb
+
+// configuration =================
+
+mongoose.connect('mongodb://bjtran:todoapp@ds035488.mongolab.com:35488/todoapp'); 	// connect to mongoDB database on modulus.io
+
+app.configure(function() {
+    app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+    app.use(express.logger('dev')); 						// log every request to the console
+    app.use(express.bodyParser()); 							// pull information from html in POST
+    app.use(express.methodOverride()); 						// simulate DELETE and PUT
+});
+
+
+// define model =================
+var Todo = mongoose.model('Todo', {
+    text : String
+});
+
+// routes ======================================================================
+
+// api ---------------------------------------------------------------------
+// get all todos
+app.get('/api/todos', function(req, res) {
+
+    // use mongoose to get all todos in the database
+    Todo.find(function(err, todos) {
+
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err)
+            res.send(err)
+
+        res.json(todos); // return all todos in JSON format
+    });
+});
+
+// create todo and send back all todos after creation
+app.post('/api/todos', function(req, res) {
+
+    // create a todo, information comes from AJAX request from Angular
+    Todo.create({
+        text : req.body.text,
+        done : false
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+
+});
+
+// delete a todo
+app.delete('/api/todos/:todo_id', function(req, res) {
+    Todo.remove({
+        _id : req.params.todo_id
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        // get and return all the todos after you create another
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+});
+
+// application -------------------------------------------------------------
+app.get('*', function(req, res) {
+    res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+});
+
+// listen (start app with node server.js) ======================================
+app.listen(8080);
+console.log("App listening on port 8080");
